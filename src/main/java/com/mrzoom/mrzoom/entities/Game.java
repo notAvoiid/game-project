dab84574fe549d84506d9edc9046ef898a29e96c
Igor Nascimento <igor.nacimento51@gmail.com>
2023-05-24 15:46:26 -0300

    Seeding.

--- src/main/java/com/mrzoom/mrzoom/entities/Game.java
+++ src/main/java/com/mrzoom/mrzoom/entities/Game.java
@@ -18,21 +18,23 @@ public class Game {
     private String genre;
     private String platforms;
     private Double score;
-    private String imgURL;
+    private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgURL, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
         this.score = score;
-        this.imgURL = imgURL;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
@@ -85,12 +87,12 @@ public class Game {
         this.score = score;
     }
 
-    public String getImgURL() {
-        return imgURL;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
-    public void setImgURL(String imgURL) {
-        this.imgURL = imgURL;
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
