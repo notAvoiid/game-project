c0f657161de3ec60cf1bcd1a493858271581e427
Igor Nascimento <igor.nacimento51@gmail.com>
2023-05-24 15:30:03 -0300

    Creating entities

---
+++ src/main/java/com/mrzoom/mrzoom/MrzoomApplication.java
@@ -0,0 +1,13 @@
+package com.mrzoom.mrzoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrzoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrzoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrzoom/mrzoom/MrzoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrzoom.mrzoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrzoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
